a9c498006b87f312eaf81ad6e0eaa817dedf834e
Akash Munagala <akash.munagala@gmail.com>
2021-08-09 23:58:09 -0700

    Add sponsors page

---
+++ src/components/SponsorItem.svelte
@@ -0,0 +1,50 @@
+<script>
+    export let big;
+    export let image;
+    export let text;
+</script>
+
+<div class="content {big ? 'big' : 'small'}">
+    <div class="image-wrapper">
+        <img class="image" src="{image}" alt="placeholder" />
+    </div>
+    <div class="text-wrapper">
+        <div class="text">{text}</div>
+    </div>
+</div>
+
+<style>
+    .content {
+        width: 100%;
+    }
+    .big {
+        flex: 5;
+    }
+    .small {
+        flex: 4;
+    }
+    .image-wrapper {
+        display: flex;
+        justify-content: center;
+        margin-bottom: -15%;
+    }
+    .image {
+        border: solid 1px white;
+        border-radius: 10px;
+        margin: 0 5% 0 5%;
+        width: 30%;
+    }
+    .text-wrapper {
+        display: flex;
+        justify-content: center;
+        height: 80%;
+    }
+    .text {
+        border: solid 1px white;
+        border-radius: 10px;
+        margin: 5%;
+        font-size: 0.7em;
+        width: 60%;
+        padding: 15% 5% 15% 5%;
+    }
+</style>
--- src/pages/Sponsors.svelte
+++ src/pages/Sponsors.svelte
@@ -1,1 +1,39 @@
-<h1>hello from sponsors</h1>
+<script>
+    import Modal from "../components/Modal.svelte";
+    import SponsorItem from "../components/SponsorItem.svelte";
+</script>
+
+<div id="sponsors">
+    <Modal title="sponsors">
+        <div id="content">
+            <div class="sponsors-row">
+                <SponsorItem big={false} image="https://via.placeholder.com/200x200" text="My name is Cat. I am a project manager in Capple. I specialized in search optimization, particularly in the field of cat food" />
+                <SponsorItem big={true} image="https://via.placeholder.com/200x200" text="My name is Lion. I am a senior software engineer in Lion Valley. I don't know what I'm doing most of the time" />
+                <SponsorItem big={false} image="https://via.placeholder.com/200x200" text="My name is dog. I am the CEO of Dogs Inc., a company which thrives to create a more sustainable earth for future generation of canines." />
+            </div>
+            <div class="sponsors-row">
+                <SponsorItem big={false} image="https://via.placeholder.com/200x200" text="My name is Cat. I am a project manager in Capple. I specialized in search optimization, particularly in the field of cat food" />
+                <SponsorItem big={true} image="https://via.placeholder.com/200x200" text="My name is Lion. I am a senior software engineer in Lion Valley. I don't know what I'm doing most of the time" />
+                <SponsorItem big={false} image="https://via.placeholder.com/200x200" text="My name is dog. I am the CEO of Dogs Inc., a company which thrives to create a more sustainable earth for future generation of canines." />
+            </div>
+        </div>
+    </Modal>
+</div>
+
+<style>
+    #sponsors {
+        background-color: #121212;
+        max-width: 100vw;
+        min-height: 100vh;
+        padding: 10vh 10vw;
+    }
+    #content {
+        display: flex;
+        flex-direction: column;
+        overflow: auto;
+        height: 50vh;
+    }
+    .sponsors-row {
+        display: flex;
+    }
+</style>
