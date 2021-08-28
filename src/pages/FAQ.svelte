dd100b0ffd44616c138b0f9b3c1bb50fc96c8e9a
Chinchuluun1029 <chinchuluun1029@gmail.com>
2021-08-28 16:08:45 -0500

    Adding scroll for FAQ

--- src/pages/FAQ.svelte
+++ src/pages/FAQ.svelte
@@ -5,14 +5,16 @@
 
 <div id="faq">
     <Modal title="FAQ">
-        <ul id="list">
-            <FAQItem question="Is this a virtual event?" answer="This is the answer" />
-            <FAQItem question="Can non-Vandy students join?" answer="This is the answer" />
-            <FAQItem question="Will there be food?" answer="Yes! We have partnered up with local catering services to provide you lunch and dinner! (No breakfast, though, you're on your own!)" />
-            <FAQItem question="How many people can be on my team?" answer="This is the answer" />
-            <FAQItem question="What are the prizes?" answer="This is the answer" />
-            <FAQItem question="I don't know how to code. Are there any workshops?" answer="This is the answer" />
-        </ul>
+        <div id="list-container">
+            <ul id="list">
+                <FAQItem question="Is this a virtual event?" answer="This is the answer" />
+                <FAQItem question="Can non-Vandy students join?" answer="This is the answer" />
+                <FAQItem question="Will there be food?" answer="Yes! We have partnered up with local catering services to provide you lunch and dinner! (No breakfast, though, you're on your own!)" />
+                <FAQItem question="How many people can be on my team?" answer="This is the answer" />
+                <FAQItem question="What are the prizes?" answer="This is the answer" />
+                <FAQItem question="I don't know how to code. Are there any workshops?" answer="This is the answer" />
+            </ul>
+        </div>
     </Modal>
 </div>
 
@@ -22,6 +24,10 @@
         max-width: 100vw;
         min-height: 100vh;
         padding: 10vh 10vw;
+    }
+
+    #list-container {
+        overflow: auto;
     }
 
     #list {
