bb41a15ee14267ff69ddd049cdbb4071340428c6
Chinchuluun1029 <chinchuluun1029@gmail.com>
2021-08-28 15:00:14 -0500

    Baseline alignment, removing ={true}

--- src/pages/Sponsors.svelte
+++ src/pages/Sponsors.svelte
@@ -4,16 +4,16 @@
 </script>
 
 <div id="sponsors">
-    <Modal title="sponsors">
+    <Modal title="SPONSORS">
         <div id="content">
             <div class="sponsors-row">
                 <SponsorItem big={false} image="https://via.placeholder.com/200x200" text="My name is Cat. I am a project manager in Capple. I specialized in search optimization, particularly in the field of cat food" />
-                <SponsorItem big={true} image="https://via.placeholder.com/200x200" text="My name is Lion. I am a senior software engineer in Lion Valley. I don't know what I'm doing most of the time" />
+                <SponsorItem big image="https://via.placeholder.com/200x200" text="My name is Lion. I am a senior software engineer in Lion Valley. I don't know what I'm doing most of the time" />
                 <SponsorItem big={false} image="https://via.placeholder.com/200x200" text="My name is dog. I am the CEO of Dogs Inc., a company which thrives to create a more sustainable earth for future generation of canines." />
             </div>
             <div class="sponsors-row">
                 <SponsorItem big={false} image="https://via.placeholder.com/200x200" text="My name is Cat. I am a project manager in Capple. I specialized in search optimization, particularly in the field of cat food" />
-                <SponsorItem big={true} image="https://via.placeholder.com/200x200" text="My name is Lion. I am a senior software engineer in Lion Valley. I don't know what I'm doing most of the time" />
+                <SponsorItem big image="https://via.placeholder.com/200x200" text="My name is Lion. I am a senior software engineer in Lion Valley. I don't know what I'm doing most of the time" />
                 <SponsorItem big={false} image="https://via.placeholder.com/200x200" text="My name is dog. I am the CEO of Dogs Inc., a company which thrives to create a more sustainable earth for future generation of canines." />
             </div>
         </div>
@@ -35,5 +35,6 @@
     }
     .sponsors-row {
         display: flex;
+        align-items: baseline;
     }
 </style>
