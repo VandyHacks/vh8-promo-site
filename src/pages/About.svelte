471b0b037f273137738cce90cd43b79930600325
Chinchuluun1029 <chinchuluun1029@gmail.com>
2021-08-28 16:09:12 -0500

    Formatting

--- src/pages/About.svelte
+++ src/pages/About.svelte
@@ -9,9 +9,10 @@
             <li>Date: October 10, 2021</li>
             <li>Location: The Wond'ry</li>
             <li>
-                Details: Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio quae deleniti 
-                perspiciatis tempore porro. Officiis laboriosam quos consectetur sed reprehenderit 
-                consequuntur, placeat hic in! Iste, id facere? Dolore, eum quae!
+                Details: Lorem ipsum dolor sit amet consectetur adipisicing
+                elit. Optio quae deleniti perspiciatis tempore porro. Officiis
+                laboriosam quos consectetur sed reprehenderit consequuntur,
+                placeat hic in! Iste, id facere? Dolore, eum quae!
             </li>
         </ul>
     </Modal>
@@ -23,7 +24,9 @@
         max-width: 100vw;
         min-height: 100vh;
         padding: 10vh 10vw;
+        box-sizing: border-box;
     }
+    
     ul {
         margin-left: 5vw;
     }
