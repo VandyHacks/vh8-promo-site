4e025d8f3445d9b2d9b5d5e39843076cce60382e
Akash Munagala <akash.munagala@gmail.com>
2021-09-06 16:10:00 -0500

    Pink planet changes - from Aadi

--- src/components/Layout.svelte
+++ src/components/Layout.svelte
@@ -7,7 +7,7 @@
 
 <div class:hasActive={$hasActive}>
     <Button id="about-button" image="bluePlanet.svg" name="about" active={$currentlyActive === "about"}/>
-    <Button id="faq-button" image="dustPlanet.svg" name="faq" active={$currentlyActive === "faq"}/>
+    <Button id="faq-button" image="pinkPlanet.svg" name="faq" active={$currentlyActive === "faq"}/>
     <Button id="sponsors-button" image="orangePlanet.svg" name="sponsors" active={$currentlyActive === "sponsors"}/>
     <Button id="winners-button" image="redPlanet.svg" name="winners" url="https://vandyhacks-retro-edn.devpost.com/project-gallery" hasPlanetEffect={false} active={false}/>
 	<div class="content">
