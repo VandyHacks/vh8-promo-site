4fe80f558afdcc75dd15f87fe0f7436e465a7b4d
Chinchuluun1029 <chinchuluun1029@gmail.com>
2021-10-08 22:03:18 -0500

    Spacing

--- src/components/Timeline.svelte
+++ src/components/Timeline.svelte
@@ -13,8 +13,7 @@
                     headers: {
                         "Content-Type": "application/json",
                         "Access-Control-Allow-Origin": "*",
-                        "Access-Control-Allow-Headers":
-                        "Origin, X-Requested-With, Content-Type, Accept",
+                        "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
                     },
                 });
             const items = await res.json();
