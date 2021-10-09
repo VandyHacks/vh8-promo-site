2caa743eb4d0953b2fe8a5bf9947785b8e708297
Chinchuluun1029 <chinchuluun1029@gmail.com>
2021-10-08 19:13:06 -0500

    Fixing view for mobile, retrying fetch

--- src/components/Timeline.svelte
+++ src/components/Timeline.svelte
@@ -1,21 +1,35 @@
 <script>
     import { onMount } from "svelte";
-    import { scheduleItems } from '../stores.js';
-    import { get } from 'svelte/store';
+    import { scheduleItems } from "../stores.js";
+    import { get } from "svelte/store";
 
     onMount(async () => {
-        fetch("https://apply.vandyhacks.org/api/manage/events/pull", {
-            credentials: "include"
-        })
-        .then(response => response.json())
-        .then(data => {
-            scheduleItems.set(data);
-        }).catch(_ => {
+        try {
+            const fetchRes = await fetch(
+                "https://apply.vandyhacks.org/api/manage/events/pull",
+                {
+                    method: "GET",
+                    mode: "no-cors",
+                    credentials: "same-origin",
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Access-Control-Allow-Origin": "*",
+                        "Access-Control-Allow-Headers":
+                            "Origin, X-Requested-With, Content-Type, Accept",
+                    },
+                    referrerPolicy: "no-referrer",
+                }
+            );
+            const response = await fetchRes.json();
+            scheduleItems.set(response);
+        } catch (e) {
             console.error("Failed to fetch latest events, showing fallback");
-        });
+        }
     });
 
-    const sortedSchedule = get(scheduleItems).sort((a, b) => new Date(a.startTimestamp) - new Date(b.startTimestamp));
+    const sortedSchedule = get(scheduleItems).sort(
+        (a, b) => new Date(a.startTimestamp) - new Date(b.startTimestamp)
+    );
 
     const isValidUrl = (string) => {
         let url;
@@ -45,11 +59,13 @@
         const endDate = new Date(startDate.getTime() + duration * 60000);
 
         const startTime = `${
-            startDate.getHours() < 10 ? "0" : ""}${startDate.getHours()}:${
+            startDate.getHours() < 10 ? "0" : ""
+        }${startDate.getHours()}:${
             startDate.getMinutes() < 10 ? "0" : ""
         }${startDate.getMinutes()}`;
         const endTime = `${
-            endDate.getHours() < 10 ? "0" : ""}${endDate.getHours()}:${
+            endDate.getHours() < 10 ? "0" : ""
+        }${endDate.getHours()}:${
             endDate.getMinutes() < 10 ? "0" : ""
         }${endDate.getMinutes()}`;
 
@@ -232,5 +248,46 @@
         );
 
         z-index: 5;
+    }
+
+    @media only screen and (max-width: 420px) {
+        * {
+            font-size: 1rem;
+        }
+        .bubble {
+            margin-left: 0px;
+        }
+
+        .left {
+            padding: 24px;
+            min-width: calc(100% - 24px);
+            box-sizing: content-box;
+        }
+        .right {
+            min-width: calc(100% - 24px);
+            box-sizing: content-box;
+        }
+
+        .bubble::after {
+            content: "";
+            border: 0;
+        }
+
+        .timeline:before {
+            visibility: hidden;
+        }
+
+        .bubble {
+            flex-direction: column;
+            min-width: 100%;
+        }
+
+        .name {
+            font-size: 2.5rem;
+        }
+
+        .right {
+            border: 0;
+        }
     }
 </style>
